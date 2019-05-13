1c92ea73f53821453c267cd0fbf2a4a6549addf4
yaxlie <m.lesnierowski@gmail.com>
2019-05-14 00:40:14 +0200

    Add example recognition function

---
+++ kanji_rest/recognition/lib/example.py
@@ -0,0 +1,5 @@
+import time
+
+def check(name, image):
+    time.sleep(5)
+    return '{"answer": "Correct"}'
--- kanji_rest/recognition/views.py
+++ kanji_rest/recognition/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 import json
-import time
+
+from recognition.lib import example
 
 @csrf_exempt
 def recognize(request: HttpRequest):
 	if request.method == 'POST':
 		body = json.loads(request.body)
-		time.sleep(5)
-		return HttpResponse("Test: {}\n".format(body["image"]))
+		return HttpResponse(example.check(body["name"], body["image"]))
 	else:
 		return HttpResponse("{error}")
