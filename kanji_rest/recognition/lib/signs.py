64f1de9381e61d7b7b1d1a366889ef777556a0c7
yaxlie <m.lesnierowski@gmail.com>
2019-06-15 00:09:17 +0200

    Fix csv labels

--- kanji_rest/recognition/lib/signs.py
+++ kanji_rest/recognition/lib/signs.py
@@ -5,6 +5,7 @@ import csv
 def get_signs():
 	f = open( './k49_classmap.csv', 'rU', encoding='utf-8' )  
 	reader = csv.DictReader( f, fieldnames = ( "index","codepoint","char","rom" ))  
+	next(reader, None)
 	out = json.dumps( [ row for row in reader ] ) 
 
 	return out
