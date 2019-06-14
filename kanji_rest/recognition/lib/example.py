c28a0d76157e7f0dc6eb4a7ae9379be68be8935a
yaxlie <m.lesnierowski@gmail.com>
2019-06-14 22:12:06 +0200

    Load model only once
    * This speeds up response time greatly

--- kanji_rest/recognition/lib/example.py
+++ kanji_rest/recognition/lib/example.py
@@ -8,63 +8,81 @@ import io
 import json
 import pandas as pd
 from PIL import Image
-import scipy
+import imageio
 
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# from recognition.models import model, graph
+from django.db import models
+from tensorflow.keras.models import load_model
+import tensorflow as tf
 
+# from django.conf import settings
+
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+
+model = load_model("model/model_kanji.ckpt")
+# model.summary()
+
+graph = tf.compat.v1.get_default_graph() 
+# sess = tf.Sessi
 
 def check(label, image):
-    img = base64.b64decode(image)
-    img = io.BytesIO(img)
-    img = mpimg.imread(img, format='PNG')
+    global graph
+    global sess
+    global variables
+    with graph.as_default():
+        with sess.as_default():
+            img = base64.b64decode(image)
+            img = io.BytesIO(img)
+            img = mpimg.imread(img, format='PNG')
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    img = crop_image(255 - img)
+            img = crop_image(255 - img)
 
-    img = resize_image(img, 28, 28)
+            img = resize_image(img, 28, 28)
 
-    kernel = np.ones((3,3),np.uint8)
-    img = cv2.dilate(img, kernel)
+            kernel = np.ones((3,3),np.uint8)
+            img = cv2.dilate(img, kernel)
+            # img = cv2.filter2D(img,-1,np.ones((1,1),np.uint8))
 
-    # plt.imshow(img, interpolation='nearest')
-    # plt.show()
-    # plt.close()
+            # plt.imshow(img, interpolation='nearest')
+            # plt.show()
+            # plt.close()
 
-    classes = pd.read_csv("k49_classmap.csv")
+            classes = pd.read_csv("k49_classmap.csv")
 
-    correct = correct_classification(image, label)
-    img = img.reshape(1, 28, 28)
-    model = load_model("model/model_kanji.ckpt")
+            correct = correct_classification(image, label)
+            img = img.reshape(1, 28, 28)
 
-    # def test_gen(X_test):
-    #     datagen = ImageDataGenerator(featurewise_center=True,
-    #        featurewise_std_normalization=True)
-    #     Y_trash = np.ones(X_test.shape[0])
-    #     flow = datagen.flow(X_test, Y_trash)
-    #     for X,Y in flow:
-    #         yield X #ignore Y
-    # distr = model.predict_generator(test_gen(img), steps = 1)
-    # datagen_test = ImageDataGenerator(featurewise_center=True,
-    #   featurewise_std_normalization=True)
+            # def test_gen(X_test):
+            #     datagen = ImageDataGenerator(featurewise_center=True,
+            #        featurewise_std_normalization=True)
+            #     Y_trash = np.ones(X_test.shape[0])
+            #     flow = datagen.flow(X_test, Y_trash)
+            #     for X,Y in flow:
+            #         yield X #ignore Y
+            # distr = model.predict_generator(test_gen(img), steps = 1)
+            # datagen_test = ImageDataGenerator(featurewise_center=True,
+            #   featurewise_std_normalization=True)
 
-    # datagen_test.flow(img)
-    distr = model.predict(img)
-    predicted_label = classes['rom'][np.argmax(distr)]
-    print(predicted_label)
-    
-    # print(classes['rom'][classes['rom'] == label].index[0])
-    response = {}
-    response['correct'] = predicted_label == label
-    response['prob_correct'] = float(distr[0][classes['rom'][classes['rom'] == label].index[0]])
-    json_response = json.dumps(response)
+            # datagen_test.flow(img)
 
-    # scipy.misc.imsave('images/{}_{}_{}.png'.format(response['correct'], response['prob_correct'], label), img)
+            distr = model.predict(img)
+            predicted_label = classes['rom'][np.argmax(distr)]
+            print(predicted_label)
+            
+            # model.summary()
+            # print(classes['rom'][classes['rom'] == label].index[0])
+            response = {}
+            response['correct'] = predicted_label == label
+            response['prob_correct'] = float(distr[0][classes['rom'][classes['rom'] == label].index[0]])
+            json_response = json.dumps(response)
 
-    return json_response
+            imageio.imwrite('images/{}_{}_{}_{}.png'.format(label, predicted_label, response['correct'], response['prob_correct']), img[0, :, :])
+
+            return json_response
 
 
 def correct_classification(image, label):
